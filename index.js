0de1255ec3235399dad2f3e169851984d98fa377
MutaiNewton56 <mutainewton56.gmail.com>
2024-03-24 16:06:56 +0300

    control flow lab complete

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// function scuberGreetingForFeet(){
-//   // Write your code here!
-// }
-
-// function ternaryCheckCity(){
-//   // Write your code here!
-// }
-
-// function switchOnCharmFromTip(){
-//   // Write your code here!
-// }
-
-
 function scuberGreetingForFeet(distance) {
   if (distance <= 400) {
     return 'This one is on me!';
